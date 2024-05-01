da27591fa68cc7a383ce4bc27c66a90bbc608cf7
SpendleyT <153658001+SpendleyT@users.noreply.github.com>
2024-05-01 14:16:14 -0700

    Completed first draft of ETL methods

--- etl/common/s3.py
+++ etl/common/s3.py
@@ -2,9 +2,11 @@
 import os
 import boto3
 import logging
+import pandas as pd
 from io import StringIO, BytesIO
 from etl.common.constants import S3BucketConfigs
 
+logger = logging.getLogger(__name__)
 
 class s3BucketConnector():
     """Class for interacting with s3 Buckets"""
@@ -16,19 +18,17 @@ class s3BucketConnector():
         :param endpoint_url: s3 access url
         :bucket target bucket name
         """
-        self._logger = logging.getLogger(__name__)
         self.endpoint_url = endpoint_url
-        #self.access_key = S3BucketConfigs.ETL_ACCESS_KEY_NAME
-        #self.secret_key = S3BucketConfigs.ETL_SECRET_KEY_NAME
         self.session = boto3.Session(
-            aws_access_key_id=os.environ['AWS_ACCESS_KEY_ID'],
-            aws_secret_access_key=os.environ['AWS_SECRET_ACCESS_KEY']
+            aws_access_key_id=os.environ[S3BucketConfigs.ETL_ACCESS_KEY_NAME.value],
+            aws_secret_access_key=os.environ[S3BucketConfigs.ETL_SECRET_KEY_NAME.value]
         )
         if not endpoint_url:
             self._s3 = self.session.resource(service_name='s3')
         else:
             self._s3 = self.session.resource(service_name='s3', endpoint_url=endpoint_url)
         self._bucket = self._s3.Bucket(bucket)
+        self._client = self.session.client('s3')
 
 
     def list_files_in_prefix(self, prefix: str):
@@ -44,15 +44,45 @@ class s3BucketConnector():
             obj.key for obj 
             in self._bucket.objects.filter(Prefix=prefix) 
         ]
-        return files
+        return files[1:]
 
 
-    def read_csv_to_df(self):
-        pass
+    def read_csv_to_df(self, key: str, decoding='utf-8', sep=','):
+        csv_obj = self._bucket.Object(key=key).get().get('Body').read().decode(decoding)
+        data = StringIO(csv_obj)
+        df = pd.read_csv(data, delimiter=sep)
+        return df
 
 
-    def write_df_to_s3(self, df, key):
+    def read_prq_to_df(self, key):
+        try:
+            prq_obj = self._bucket.Object(key=key).get().get('Body').read()
+            data = BytesIO(prq_obj)
+            df = pd.read_parquet(data)
+        except: 
+            logger.info("No parquet file exists. Creating new dataframe.")
+            df = pd.DataFrame()
+        
+        return df
+
+
+    def write_df_as_csv_to_s3(self, df, key):
         out_buffer = StringIO()
         df.to_csv(out_buffer, index=False)
         self._bucket.put_object(Body=out_buffer.getvalue(), Key=key)
         return True
+
+
+    def write_df_as_prq_to_s3(self, df, key):
+        out_buffer = BytesIO()
+        df.to_parquet(out_buffer, index=False)
+        self._bucket.put_object(Body=out_buffer.getvalue(), Key=key)
+        return True
+
+
+    def move_files_to_archive(self, file_list):
+        for file in file_list:
+            copy_source = {'Bucket': S3BucketConfigs.ETL_BUCKET.value, 'Key': file}
+            self._client.copy(copy_source, S3BucketConfigs.ETL_BUCKET.value, f'archive/{file}')
+
+
--- etl/processors/etl_reports.py
+++ etl/processors/etl_reports.py
@@ -5,9 +5,11 @@ from etl.common.constants import S3BucketConfigs
 import time
 import os
 from boxoffice_api import BoxOffice
-import pandas
-    
+import pandas as pd
+from pandas import DataFrame
+
 logger = logging.getLogger(__name__)
+
 
 class BoxOfficeETL():
     """Main class for running necessary ETL processes for Box Office reporting"""
@@ -18,36 +20,70 @@ class BoxOfficeETL():
         :param year: year to be extracted
         """
         #get s3 bucket
-        #self.bucket_name = S3BucketConfigs.ETL_BUCKET
-        self.bucket = s3BucketConnector('de-etl-project')  
+        self.bucket_conn = s3BucketConnector(S3BucketConfigs.ETL_BUCKET.value)  
 
 
     def run_extract(self, year: int) -> None:
         #create box office instance
-        #apikey = S3BucketConfigs.ETL_OMDB_KEY_NAME
-        box_office = BoxOffice(api_key=os.environ['OMDB_KEY'], outputformat="DF");
-        logger.info(f"Year: {year} - {type(year)}")
+        box_office = BoxOffice(api_key=os.environ[S3BucketConfigs.ETL_OMDB_KEY_NAME.value], outputformat="DF");
+        logger.info(f"Processing year: {year}")
 
         #loop thru years and call for each week
         for i in range(1, 53):
-            key = "-".join(('data_files/box-office', str(year), str(i))) + ".csv"
+            key = "-".join((f'{year}/box-office', str(year), str(i))) + ".csv"
             try:
-                weekly_data = box_office.get_weekly(year=int(year), week=i)
-                self.bucket.write_df_to_s3(weekly_data, key)
+                weekly_data_df = box_office.get_weekly(year=int(year), week=i)
+                weekly_data_df['Reference'] = f"{year}-{i}"
+                self.bucket_conn.write_df_to_s3(weekly_data_df, key)
             except AttributeError:
                 logger.error(f"Attr Error (No Results) for {year}-{i} at " + time.ctime())
             except:
                 logger.error(f"Unknown Error for {year}-{i} " + time.ctime())
+        #Return true for confirmation
+        return True
+
+    def run_transform(self) -> DataFrame:
+        """
+        Pull unprocessed data files and write to database
+        """
+        #get files currently in data_files bucket and process
+        files_to_transform = self.bucket_conn.list_files_in_prefix('data_files')
+        full_df = pd.DataFrame()
+        for csv_file in files_to_transform:
+            movie_df = self.bucket_conn.read_csv_to_df(csv_file)
+            movie_df = movie_df.dropna()
+            movie_df = movie_df[movie_df.index < 16]
+            movies_clean_df = movie_df.drop(['LW', '%± LW', 
+                'Change', 'Title', 'Rated', 'Released', 'Director', 'Writer',
+                'Actors', 'Plot', 'Language', 'Country', 'Awards', 
+                'Metascore', 'imdbRating', 'imdbVotes', 'Type', 'DVD',
+                'BoxOffice', 'Production', 'Website', 'Response', 'Error'], axis=1)
+            movies_clean_df['Year'] = movies_clean_df['Year'].astype('float').astype('Int64')
+            #movies_clean_df[['Genre1', 'Genre2', 'Genre3']] = movies_clean_df['Genre'].str.split(',', expand=True)
+            #movies_clean_df = movies_clean_df.drop(['Genre'], axis=1)
+            movies_clean_df['Ratings'] = movies_clean_df['Ratings'].str.strip('[]')
+            for index, row in movies_clean_df.iterrows():
+                ratings = row['Ratings'].str.replace('},', '}*').split('*')
+                movies_clean_df.loc[index, 'imdb_score'] = eval(ratings[0])['Value'].split('/')[0]
+                movies_clean_df.loc[index, 'rt_score'] = eval(ratings[1])['Value'].split('/')[0].strip("%")
+                movies_clean_df.loc[index, 'meta_score'] = eval(ratings[2])['Value'].split('/')[0]
+            movies_clean_df.drop(['Ratings'], axis=1, inplace=True)
+            full_df = pd.concat([full_df, movies_clean_df], ignore_index=True)
+        #write files to archive folder
+        self.bucket_conn.move_files_to_archive(files_to_transform)
+        #return full_df for use in load method
+        return full_df
 
 
-    def run_transform(self) -> None:
-        pass
-
-
-    def run_load(self) -> None:
-        pass
-
+    def run_load(self, df) -> None:
+        stored_df = self.bucket_conn.read_prq_to_df("database/movies.parquet")
+        stored_df = pd.concat([stored_df, df], ignore_index=True)
+        stored_df = stored_df.sort_values(by=['Release', 'Weeks'], ascending=False, ignore_index=True)
+        self.bucket_conn.write_df_as_prq_to_s3(df, "database/movies.parquet")
+        
 
     def run(self, year: int) -> None:
         BoxOfficeETL.run_extract(self, year)
+        full_df = BoxOfficeETL.run_transform(self)
+        BoxOfficeETL.run_load(self, full_df)
 
